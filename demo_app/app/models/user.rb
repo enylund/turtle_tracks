06e4a81eb389e3f78eee4b603004bf8810d110a0
Will <william.zhao@yale.edu>
2013-06-03 15:44:06 -0400

    removed demo_app

--- demo_app/app/models/user.rb
+++
@@ -1,3 +0,0 @@
-class User < ActiveRecord::Base
-  attr_accessible :class_year, :name, :netid
-end
--- demo_app/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-DemoApp::Application.config.secret_token = '2c5aef148ee78cdf961765ccdb45ce15d681fbad0dc8a0c22c96fa53bdeb29b661a84d36f49af28d5dd480a2ac43290e736d1fec14e40d40bda0b68863dc6848'
