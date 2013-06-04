841c6723de0d66656d32a303971a075f893940f1
enylund <eric.nylund@yale.edu>
2013-06-04 15:12:19 -0400

    finished most functionality minus the repeat functionality

--- turtle.rb
+++ turtle.rb
@@ -1,38 +1,147 @@
+class Turtle
 
-def initialize (grid_size)
-	total_grid_size = grid_size * grid_size
-	grid = Array.new(total_grid_size," .")
-	initial_position = total_grid_size/2
-	grid[initial_position]=" X"
-	grid.each_slice(grid_size) { |x|
-	  puts x.join
-	}
+  def board
+    @board
+  end
+
+  def initialize(board_size)
+    @board = Board.new(self, board_size)
+    @y = board_size/2
+    @x = board_size/2
+    @orientation = 0
+    @board.visit(@y, @x)
+  end
+
+  def rt(degree)
+    @orientation = @orientation + degree
+  end
+
+  def lt(degree)
+    @orientation = @orientation - degree
+  end
+
+  def fd (steps)
+
+    if (@orientation == 0 || @orientation == 360)
+      steps.times do
+        @y = @y - 1
+        @board.visit(@y, @x)
+      end
+
+    elsif @orientation == 45
+      steps.times do
+        @x = @x + 1
+        @y = @y + 1
+        board.visit(@y,@x)
+      end
+    elsif @orientation == 90
+      steps.times do
+        @x = @x + 1
+        board.visit(@y, @x)
+      end
+    elsif @orientation == 135
+        steps.times do
+        @x = @x + 1
+        @y = @y - 1
+        board.visit(@y,@x)
+        end
+    elsif @orientation == 180
+        steps.times do
+        @y = @y - 1
+        board.visit(@y,@x)
+        end
+    elsif @orientation == 225
+        steps.times do
+        @x = @x - 1
+        @y = @y - 1
+        board.visit(@y,@x)
+        end
+    elsif @orientation == 270
+        steps.times do
+        @x = @x - 1
+        board.visit(@y,@x)
+        end
+    elsif @orientation == 315
+        steps.times do
+        @x = @x - 1
+        @y = @y + 1
+        board.visit(@y,@x)
+        end
+    end
+
+  end
+
+  def bk
+
+    if (@orientation == 0 || @orientation == 360)
+      steps.times do
+        @y = @y + 1
+        @board.visit(@y, @x)
+      end
+
+    elsif @orientation == 45
+      steps.times do
+        @x = @x - 1
+        @y = @y - 1
+        board.visit(@y,@x)
+      end
+    elsif @orientation == 90
+      steps.times do
+        @x = @x - 1
+        board.visit(@y, @x)
+      end
+    elsif @orientation == 135
+        steps.times do
+        @x = @x - 1
+        @y = @y + 1
+        board.visit(@y,@x)
+        end
+    elsif @orientation == 180
+        steps.times do
+        @y = @y + 1
+        board.visit(@y,@x)
+        end
+    elsif @orientation == 225
+        steps.times do
+        @x = @x + 1
+        @y = @y + 1
+        board.visit(@y,@x)
+        end
+    elsif @orientation == 270
+        steps.times do
+        @x = @x + 1
+        board.visit(@y,@x)
+        end
+    elsif @orientation == 315
+        steps.times do
+        @x = @x + 1
+        @y = @y - 1
+        board.visit(@y,@x)
+        end
+    end
+  end
 end
 
+class Board
 
+  def initialize(turtle, board_size)
+    @turtle = turtle
+    @board_size = board_size
+    @board_array = Array.new(@board_size, " .") {Array.new(@board_size, " .")}
+  end
 
+  def visit(y,x)
+    @board_array[y][x] = " X"
+  end
 
-def draw_canvas (grid_size)
-	grid = Array.new((grid_size * grid_size)," .")
-	grid.each_slice(grid_size) { |x|
-	  puts x.join
-	}
+  def draw_board
+    @board_array.each do |line|
+      print line
+      print "\n"
+    end
+  end
 end
 
-initialize (11)
-
-# draw_canvas (11)
-
-# FD x
-
-# RT x
-
-# LT x
-
-# BK x
-
-# REPEAT x
-
-# out_file = File.open("simple_out.txt", "w")
-# out_file.puts draw_canvas
-# out_file.close
+my_turtle = Turtle.new(11)
+my_turtle.fd(5)
+my_turtle.board.draw_board
